43b84bfe87bf247225196440114d28f78581a046
Alexandr Chepak <djalexbc@gmail.com>
2021-01-27 21:51:17 +0200

    add componentDidMount

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,16 @@ import Modal from '../Modal/Modal';
 
 class App extends Component {
     state = {
+        image: null,
+        loading: false,
         showModal: false
+    }
+
+    componentDidMount() {
+        this.setState({loading: true});
+        fetch('https://pixabay.com/api/?q=yellow+flower&page=1&key=19201386-219bb908558d098454af3ef41&image_type=photo&orientation=horizontal&per_page=12')
+            .then(res => res.json())
+            .then(image => this.setState({ image })).finally(() => this.setState({loading: false}));
     }
 
     toggleModal = () => {
@@ -18,7 +27,9 @@ class App extends Component {
         const { showModal } = this.state;
 
         return (
+            
             <div>
+                {this.state.image && (<div>{this.state.image.hits[1].previewURL}</div>)}
                 <button type="button" onClick={this.toggleModal} className="Button">
                     Открыть
                 </button>
@@ -28,6 +39,7 @@ class App extends Component {
                 </Modal>}
 
                 <button type="button" onClick={this.toggleModal}>Закрыть</button>
+                {this.state.loading && <h1>Загружаем...</h1>}
             </div>
                 
                 
